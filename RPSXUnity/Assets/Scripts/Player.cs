402899ecf121226998c572a86cbeaca0fef39475
Unknown <andrewpetersell@gmail.com>
2017-06-02 16:50:49 -0400

    Movement Basics Working

---
+++ RPSXUnity/Assets/Scripts/Player.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	Rigidbody2D rb;
+	SpriteRenderer sr;
+
+	public int playerNum;
+
+	public float moveSpeed;
+	public float jumpSpeed;
+	public float normalGrav;
+	public float fastFallGrav;
+	public float hoverGrav;
+	public float gravityThreshold;
+
+	public int maxJumps; 
+	public int jumpsRemaining;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D> ();
+		sr = GetComponent<SpriteRenderer> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		movement ();
+		
+	}
+		
+	void movement ()
+	{
+//		Left and right movement.
+		if (Input.GetAxis ("LeftStickX_P" + playerNum) > 0) 
+		{
+			rb.velocity = new Vector2 (moveSpeed, rb.velocity.y);
+			sr.flipX = false;
+		}
+
+		if (Input.GetAxis ("LeftStickX_P" + playerNum) < 0) 
+		{
+			rb.velocity = new Vector2 ((moveSpeed * -1), rb.velocity.y);
+			sr.flipX = true;
+		}
+
+		if (Input.GetAxis ("LeftStickX_P" + playerNum) == 0)
+		{
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+
+//		Jumping
+		if (Input.GetButtonDown ("AButton_P" + playerNum)) 
+		{
+			if (jumpsRemaining != 0) 
+			{
+				rb.velocity = new Vector2 (rb.velocity.x, jumpSpeed);
+				jumpsRemaining = jumpsRemaining - 1;
+			}
+		}
+
+		//FastFalling
+		if (Input.GetAxis ("LeftStickY_P" + playerNum) > gravityThreshold) {
+			rb.gravityScale = fastFallGrav;
+		} else {
+			rb.gravityScale = normalGrav;
+		}
+	}
+
+
+	void OnCollisionEnter2D (Collision2D coll)
+	{
+		//Reset Jumps upon touching ground
+		if (coll.gameObject.tag == "Floor") 
+		{
+			jumpsRemaining = maxJumps; 
+		}
+	}
+}
